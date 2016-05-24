967b830b1664a843b409125535c56da0d9249193
Peach Wang <peachwang@outlook.com>
2016-05-24 17:07:17 +0800

    add functions: column, find, sign, perform_projection, safe, map_to

--- src/Base/util.py
+++ src/Base/util.py
@@ -13,16 +13,42 @@ from math import *
 from copy import *
 
 # @todo: add comments for the following functions
+# ================= Exception =================
+
+class UserTypeError(TypeError):
+
+    def __init__(self, field_name, field_value, expected_types) :
+        self.fieldName  = field_name
+        self.fieldValue = field_value
+        self.expectedTypes = expected_types
+        if type(self.expectedTypes) is type :
+            self.expectedTypes = [self.expectedTypes]
+        if type(self.expectedTypes) is not list :
+            raise UserTypeError('expected_types', expected_types, [type, list])
+        if map(type, self.expectedTypes).count(type) != len(self.expectedTypes) :
+            raise Exception('Expected_types does not contain just types.')
+        self.value = self.__str__()
+
+    def __str__(self) :
+        expected_types = ' or '.join([self.getTypeStr(expected_type) for expected_type in self.expectedTypes])
+        return 'Unexpected type(%s) of %s is given, but type(%s) is expected.' \
+            % (self.getTypeStr(self.fieldValue), self.fieldName, expected_types)
+
+    def getTypeStr(self, var_type) :
+        return re.findall('\'([^\']+)\'', str(type(var_type)))[0]
+
 
 # ==================== Web ====================
-def request(url, getData = None, postData = None, timeout = None) :
-    num_timeout, num_unknown, num_other = 0, 0, 0
+def request(url, getData = None, post_data = None, timeout = None, method = 'GET') :
+    if method == 'GET' and post_data is not None :
+        method = 'POST'
     try :
-        if getData != None :
+        if getData is not None :
             url += '?' + '&'.join(['%s=%s' % (str(key), str(value)) for key, value in getData.items()])
         data = None
-        if postData != None :
-            data = j(postData, indent = 0)
+        if post_data is not None :
+            # data = re.sub('\n', '', j(post_data, indent = 0))
+            data = j(safe(post_data), indent = 0)
         request = urllib2.Request(url, data = data)
         response = urllib2.urlopen(request, timeout = timeout)
         content = response.read()
@@ -40,6 +66,7 @@ def request(url, getData = None, postData = None, timeout = None) :
     except KeyboardInterrupt, e :
         raise
     except Exception, e:
+        print post_data
         print '[Unknown Exception]', e
         return {'e' : 'UNKNOWN', 'content' : None}
     else :
@@ -57,6 +84,19 @@ def union(*dicts) :
     for __ in dicts : _.update(__)
     return _
 
+def map_to(field_names, field_values) :
+    if type(field_names) is not list :
+        raise UserTypeError('field_names', field_names, list)
+    if type(field_values) in [int, float, bool, str, unicode] :
+        return dict(zip(field_names, [field_values] * len(field_names)))
+    elif type(field_values) is list :
+        if len(field_values) == len(field_names) :
+            return dict(zip(field_names, field_values))
+        else :
+            raise Exception('Lengths of field_names and field_values do not equal.')
+    else :
+        raise UserTypeError('field_values', field_values, [int, float, bool, str, unicode, list])
+
 # ==================== String ====================
 def strip(data, chars = ' \n\t', encoding = 'utf-8') :
     if type(data) == str :
@@ -64,17 +104,41 @@ def strip(data, chars = ' \n\t', encoding = 'utf-8') :
     elif type(data) == unicode :
         return data.encode(encoding).strip(chars).decode(encoding)
     elif type(data) == list :
-        return [strip(datum, chars) for datum in data]
+        return [strip(datum, chars, encoding) for datum in data]
     elif type(data) == tuple :
-        return (strip(datum, chars) for datum in data)
+        return (strip(datum, chars, encoding) for datum in data)
     elif type(data) == set :
-        return set([strip(datum, chars) for datum in data])
+        return set([strip(datum, chars, encoding) for datum in data])
     elif type(data) == dict :
-        return dict([(key, strip(data[key], chars)) for key in data.keys()])
+        return dict([(key, strip(data[key], chars, encoding)) for key in data.keys()])
     elif type(data) in [int, float, bool] :
         return data
     else :
-        raise Exception('strip: unknown data type')
+        raise UserTypeError('data', data, [str, unicode, list, tuple, set, dict, int, float, bool])
+
+def safe(data, encoding = 'utf-8') :
+    try :
+        if type(data) == str :
+            return data
+        elif type(data) == unicode :
+            return data.encode(encoding)
+        elif type(data) == list :
+            return [safe(datum, encoding) for datum in data]
+        elif type(data) == tuple :
+            return (safe(datum, encoding) for datum in data)
+        elif type(data) == set :
+            return set([safe(datum, encoding) for datum in data])
+        elif type(data) == dict :
+            return dict([(safe(key, encoding), safe(data[key], encoding)) for key in data.keys()])
+        elif type(data) in [int, float, bool] :
+            return data
+        else :
+            raise UserTypeError('data', data, [str, unicode, list, tuple, set, dict, int, float, bool])
+    except Exception, e :
+        print type(data)
+        print [data]
+        raise e
+        exit()
 
 def contains_empty_string(data) :
     if type(data) in [str, unicode] :
@@ -95,7 +159,7 @@ def contains_empty_string(data) :
     elif type(data) in [int, float, bool] :
         return False
     else :
-        raise Exception('containsEmptyString: unknown data type')
+        raise UserTypeError('data', data, [str, unicode, list, tuple, set, dict, int, float, bool])
 
 def str_object(data) :
     if type(data) in [str, unicode, int, float, bool] :
@@ -110,11 +174,6 @@ def str_object(data) :
         return dict([(str_object(key), str_object(data[key])) for key in data.keys()])
     else :
         return str(data)
-
-def safe(st, encoding = 'utf-8') :
-    st = strip(st)
-    st = st.decode(encoding)
-    return st
 
 def unicode_to_url_hex(st) :
     res = ''
@@ -136,6 +195,27 @@ def safe_print(stream, st, encoding = 'utf-8') :
             except Exception, e :
                 stream.write(ch)
     stream.flush()
+
+# =================== Matrix ===================
+
+def column(matrix, column_names, set_default = False, default = None) :
+    # @todo: use find
+    expected_types = [list, str, unicode, int, float, bool]
+    if type(column_names) not in expected_types :
+        raise UserTypeError('column_names', column_names, expected_types)
+    if type(column_names) is not list :
+        column_names = [column_names]
+    return find(matrix, {}, map_to(column_names, 1))
+    result = []
+    for row in matrix :
+        if row.has_key(column_name) is True :
+            result.append(row[column_name])
+        else :
+            if set_default is False :
+                return False
+            else :
+                result.append(default)
+    return result
 
 # ==================== Date ====================
 def get_date_str(timestamp = None) :
@@ -225,32 +305,55 @@ def dump_txt(fout, data, fields = None, primary_key = None, is_matrix = False, s
 def load_json(fin, object_hook = None, encoding = 'utf-8') :
     return json.loads(''.join([line.strip('\n') for line in fin.readlines()]), object_hook = object_hook, encoding = encoding)
 
-def check_criterion(data, criterion = None) :
+def validate_criterion(data, criterion = None) :
     if criterion is None : return True
-    for key, value in criterion.items() :
-        if data.get(key) is None : return False
-        if type(value) == dict :
-            if not check_criterion(data[key], value) : return False
-        else :
-            # TODO
-            if data[key] != value : return False
-    return True
+    if criterion == {} : return True
+    raise Exception('Can not handle criterion: %s.' % str(criterion))
 
-def perform_projection(data, projection = None) :
+def perform_projection(data, projection = None, raise_empty_exception = False) :
+    if type(data) is not dict :
+        raise UserTypeError('data', data, dict)
     if projection is None : return data
-    res = {}
-    for key, value in projection.items() :
-        if data.get(key) is None : continue
-        if value == True : res[key] = data[key]
+    if type(projection) is not dict :
+        raise UserTypeError('projection', projection, dict)
+    elif not (projection.values().count(1) == len(projection) \
+        or projection.values().count(0) == len(projection)) :
+        raise Exception('Projection cannot have a mix of inclusion and exclusion.')
+    else :
+        if projection == {} : return data
+        # @todo: recursively...
+        is_inclusion_mode = bool(sign(sum(projection.values())))
+        if is_inclusion_mode is True :
+            result = {}
+            for field_name in projection.keys() :
+                if data.has_key(field_name) is True :
+                    result[field_name] = data[field_name]
+                else :
+                    if raise_empty_exception is True :
+                        raise Exception('Field %s is empty, thus can not be included.' % str(field_name))
         else :
-            if type(value) == dict :
-                res[key] = perform_projection(data[key], value)
-            else : raise BaseException('Unknown projection : ' + str(value))
-    return res
+            result = data
+            for field_name in projection.keys() :
+                if data.has_key(field_name) is True :
+                    data.pop(field_name)
+                else :
+                    if raise_empty_exception is True :
+                        raise Exception('Field %s is empty, thus can not be excluded.' % str(field_name))
+        return result
+
+def perform_cast(data, cast) :
+    pass
 
 def find(data, criterion = None, projection = None) :
-    if not check_criterion(data, criterion) : return False
-    else : return perform_projection(data, projection)
+    if type(data) is not list :
+        raise UserTypeError('data', data, list)
+    if map(type, data).count(dict) != len(data) :
+        raise Exception('Data does not contain just dicts.')
+    result = []
+    for datum in data :
+        if validate_criterion(datum, criterion) is True:
+            result.append(perform_projection(datum, projection))
+    return result
 
 # ==================== File ====================
 def split_filename(filename) :
@@ -270,7 +373,7 @@ def change_ext(filename, ext) :
     _ = split_filename(filename)
     return _[0] + _[1] + ('' if _[2] == '' else '.') + ext
 
-# ==================== Statistics ====================
+# ==================== Math ====================
 def calc_mean(data) :
     # data = map(long, list(data))
     # tot = long(0)
@@ -281,6 +384,11 @@ def calc_mean(data) :
 def calc_std(data) :
     mean = calc_mean(data)
     return sqrt(1.0 * sum([(datum - mean) * (datum - mean) for datum in data]) / len(data))
+
+def sign(number) :
+    if type(number) not in [int, float] :
+        raise UserTypeError('number', number, [int, float])
+    return 0 if number == 0 else int(abs(number) / number)
 
 # ==================== System ====================
 def parse_argv(argv) :
